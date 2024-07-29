e4180cde4f0c01cb4ec5026fa0d6df526de9a5e6
OscarAlarcon99 <oscaralarcon9906@gmail.com>
2024-07-28 19:20:29 -0500

    secund step

---
+++ EmotivEpocVRProject/Assets/VRTemplateAssets/Scripts/GrapichTimeLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using Unity.VRTemplate;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace Assets.VRTemplateAssets.Scripts
+{
+    public class GrapichTimeLine : MonoBehaviour
+    {
+        public RectTransform graphContainer;
+        public RectTransform labelTemplateX;
+        public RectTransform doteTemplate;
+        public RectTransform labelTemplateY;
+        public VideoTimeScrubControl videoController;
+
+        public void CreateTimeLine(float time)
+        {
+            float videoDurationInSeconds = (float) videoController.m_VideoPlayer.clip.length;
+            Debug.Log("Time " + videoDurationInSeconds);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
